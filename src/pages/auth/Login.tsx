b37c7d5a094d553400acdfe5ac39c8a0505439d2
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-08-15 15:37:25 +0200

    I1BCIP-10: user can't register by Google if there is users exist in the database

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -11,163 +11,186 @@ import { AuthUrl, LoginUrl, AuthConfigUrl } from '../../services/ApiUrls';
 import '../../styles/style.css';
 
 export default function Login() {
-    const navigate = useNavigate();
-    const [token, setToken] = useState(false);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [isGoogleLoginEnabled, setIsGoogleLoginEnabled] = useState(false);
+  const navigate = useNavigate();
+  const [token, setToken] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isGoogleLoginEnabled, setIsGoogleLoginEnabled] = useState(false);
 
-    useEffect(() => {
-        if (localStorage.getItem('Token')) {
-            navigate('/app');
-        }
-    }, [token]);
+  useEffect(() => {
+    if (localStorage.getItem('Token')) {
+      navigate('/app');
+    }
+  }, [token]);
 
-    useEffect(() => {
-        // Fetch auth-config to check if Google login is enabled
-        const head = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        };
-    
-        // Fetch auth-config without a body for GET request
-        fetchData(AuthConfigUrl, 'GET', null as any, head) // Pass headers as the fourth argument
-            .then((res) => {
-                if (res.data && typeof res.data.is_google_login === 'boolean') {
-                    setIsGoogleLoginEnabled(res.data.is_google_login); // Set the state based on response
-                } else {
-                    console.error('Invalid response format from auth-config');
-                }
-            })
-            .catch((error) => {
-                console.error('Error fetching auth-config:', error);
-            });
-    }, []);
-
-    const loginWithGoogle = useGoogleLogin({
-        onSuccess: tokenResponse => {
-            const apiToken = { token: tokenResponse.access_token };
-            const head = {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            };
-            fetchData(`${AuthUrl}/`, 'POST', JSON.stringify(apiToken), head)
-                .then((res) => {
-                    localStorage.setItem('Token', `Bearer ${res.access_token}`);
-                    setToken(true);
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
-        },
-    });
-
-    const handleLogin = (e: any) => {
-        e.preventDefault();
-        const payload = { email, password }; // Ensure you have these defined in your component
-        const head = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        };
-    
-        fetchData("auth/login/", 'POST', JSON.stringify(payload), head)
-            .then((res) => {
-                if (res && res.access) { // Check if the access token is present
-                    localStorage.setItem('Token', `Bearer ${res.access}`); 
-                    setToken(true);  // Set token state to true (you might want to handle redirect here)
-                    navigate('/app'); // Redirect to your app after successful login
-                } else {
-                    setError('Invalid response format');
-                }
-            })
-            .catch((error) => {
-                setError('Invalid username or password'); // Set error state for UI feedback
-                console.error('Error:', error);
-            });
+  useEffect(() => {
+    const head = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     };
 
-    return (
-        <div>
-            <Stack
-                direction={{ xs: 'column', sm: 'row' }}
-                justifyContent='center'
-                alignItems='center'
-                sx={{ height: '100%', width: '100%', position: 'fixed' }}
-            >
-                <Grid
-                    container
-                    item
-                    xs={8}
-                    direction='column'
-                    justifyContent='space-evenly'
-                    alignItems='center'
-                    sx={{ height: '100%', overflow: 'hidden' }}
-                >
-                    <Grid item>
-                        <Grid sx={{ mt: 2 }}>
-                            <img src={imgLogo} alt='register_logo' className='register-logo' />
-                        </Grid>
-                        <Typography variant='h5' style={{ fontWeight: 'bolder' }}>Sign In</Typography>
-                        <Grid item sx={{ mt: 4 }}>
-                            <form onSubmit={handleLogin}>
-                                <TextField
-                                    label="Email"
-                                    variant="outlined"
-                                    fullWidth
-                                    required
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    sx={{ mb: 2 }}
-                                />
-                                <TextField
-                                    label="Password"
-                                    type="password"
-                                    variant="outlined"
-                                    fullWidth
-                                    required
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    sx={{ mb: 2 }}
-                                />
-                                <Button type="submit" variant="contained" color="primary" sx={{ mb: 2 }}>Login</Button>
-                                {error && <Typography color="error">{error}</Typography>}
-                            </form>
+    fetchData(AuthConfigUrl, 'GET', null as any, head)
+      .then((res) => {
+        if (res.data && typeof res.data.is_google_login === 'boolean') {
+          setIsGoogleLoginEnabled(res.data.is_google_login);
+        } else {
+          console.error('Invalid response format from auth-config');
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching auth-config:', error);
+      });
+  }, []);
 
-                                <GoogleButton variant='outlined' onClick={() => loginWithGoogle()} sx={{ fontSize: '12px', fontWeight: 500 }}>
-                                    Sign in with Google
-                                    <img src={imgGoogle} alt='google' style={{ width: '17px', marginLeft: '5px' }} />
-                                </GoogleButton>
-                            
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid
-                    container
-                    item
-                    xs={8}
-                    direction='column'
-                    justifyContent='center'
-                    alignItems='center'
-                    className='rightBg'
-                    sx={{ height: '100%', overflow: 'hidden', justifyItems: 'center' }}
+  const loginWithGoogle = useGoogleLogin({
+    onSuccess: (tokenResponse) => {
+      const apiToken = { token: tokenResponse.access_token };
+      const head = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      };
+      fetchData(`${AuthUrl}/`, 'POST', JSON.stringify(apiToken), head)
+        .then((res) => {
+          if (res.access_token) {
+            localStorage.setItem('Token', `Bearer ${res.access_token}`);
+            setToken(true);
+          } else {
+            setError(res.message);
+          }
+        })
+        .catch((error) => {
+          setError('Error logging in with Google.');
+          console.error('Error:', error);
+        });
+    },
+  });
+
+  const handleLogin = (e: any) => {
+    e.preventDefault();
+    const payload = { email, password };
+    const head = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    };
+
+    fetchData('auth/login/', 'POST', JSON.stringify(payload), head)
+      .then((res) => {
+        if (res && res.access) {
+          localStorage.setItem('Token', `Bearer ${res.access}`);
+          setToken(true);
+          navigate('/app');
+        } else {
+          setError('Invalid response format');
+        }
+      })
+      .catch((error) => {
+        setError('Invalid username or password');
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <div>
+      <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        justifyContent="center"
+        alignItems="center"
+        sx={{ height: '100%', width: '100%', position: 'fixed' }}
+      >
+        <Grid
+          container
+          item
+          xs={8}
+          direction="column"
+          justifyContent="space-evenly"
+          alignItems="center"
+          sx={{ height: '100%', overflow: 'hidden' }}
+        >
+          <Grid item>
+            <Grid sx={{ mt: 2 }}>
+              <img
+                src={imgLogo}
+                alt="register_logo"
+                className="register-logo"
+              />
+            </Grid>
+            <Typography variant="h5" style={{ fontWeight: 'bolder' }}>
+              Sign In
+            </Typography>
+            <Grid item sx={{ mt: 4 }}>
+              <form onSubmit={handleLogin}>
+                <TextField
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  sx={{ mb: 2 }}
+                />
+                <TextField
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  sx={{ mb: 2 }}
+                />
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  sx={{ mb: 2 }}
                 >
-                    <Grid item >
-                        <Stack sx={{ alignItems: 'center' }}>
-                            <h3>Welcome to BottleCRM</h3>
-                            <p> Free and OpenSource CRM from small medium business.</p>
-                            <img
-                                src={imgLogin}
-                                alt='register_ad_image'
-                                className='register-ad-image'
-                            />
-                            <footer className='register-footer'>
-                                bottlecrm.com
-                            </footer>
-                        </Stack>
-                    </Grid>
-                </Grid>
+                  Login
+                </Button>
+              </form>
+
+              {isGoogleLoginEnabled && (
+                <GoogleButton
+                  variant="outlined"
+                  onClick={() => loginWithGoogle()}
+                  sx={{ fontSize: '12px', fontWeight: 500 }}
+                >
+                  Sign in with Google
+                  <img
+                    src={imgGoogle}
+                    alt="google"
+                    style={{ width: '17px', marginLeft: '5px' }}
+                  />
+                </GoogleButton>
+              )}
+              {error && <Typography color="error">{error}</Typography>}
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          item
+          xs={8}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          className="rightBg"
+          sx={{ height: '100%', overflow: 'hidden', justifyItems: 'center' }}
+        >
+          <Grid item>
+            <Stack sx={{ alignItems: 'center' }}>
+              <h3>Welcome to BottleCRM</h3>
+              <p> Free and OpenSource CRM for small medium business.</p>
+              <img
+                src={imgLogin}
+                alt="register_ad_image"
+                className="register-ad-image"
+              />
+              <footer className="register-footer">bottlecrm.com</footer>
             </Stack>
-        </div>
-    );
+          </Grid>
+        </Grid>
+      </Stack>
+    </div>
+  );
 }
