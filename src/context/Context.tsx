a50f910752765c35185bd94c9b4e7217faacf5ae
gaidds <ad.gaidamovich@gmail.com>
2024-08-27 13:15:58 +0200

    add context for setUserRole and userRole

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 // interface MyContextData {
 //   screen: string;
@@ -6,8 +6,23 @@ import { createContext, useContext } from 'react';
   // count: number;
   // updateCount: (newCount: number) => void;
 // }
+interface MyContextData {
+  userRole: string | null;
+  setUserRole: (role: string | null) => void;
+}
 
 const MyContext = createContext<any>(undefined);
+
+export const MyContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [userRole, setUserRole] = useState<string | null>(null);
+
+  return (
+      <MyContext.Provider value={{  userRole, setUserRole }}>
+          {children}
+      </MyContext.Provider>
+  );
+};
+
 
 export const useMyContext = () => {
   const context = useContext(MyContext);
