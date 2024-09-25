537358fbb770154cd59e3243d3495ecd8f2fe5d0
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-09-26 01:13:07 +0200

    I1BCIP-25: Created the basic of profile page

---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import './styles.css';
+
+const ProfilePage: FC = () => {
+  return <div className="profile-page-container">Profile Page</div>;
+};
+
+export default ProfilePage;
