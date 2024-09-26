6408fc00be9ea988e90edfd471b48cafff6ec330
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-09-26 15:20:21 +0200

    I1BCIP-25: Created profile page layout

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -3,7 +3,17 @@ import './styles.css';
 import { UserDataProps } from './types';
 
 const ProfilePage: FC<UserDataProps> = ({ userData }) => {
-  return <div className="profile-page-container">Profile Page</div>;
+  return (
+    <div className="profile-page-container">
+      <div className="profile-page-header">
+        <p>Edit</p>
+      </div>
+      <div className="profile-page-body">
+        <div className="profile-page-left-section"></div>
+        <div className="profile-page-right-section"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
