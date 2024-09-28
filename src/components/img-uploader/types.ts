8d977ebb095b66c20181ac9133cb7a1806dccced
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-09-29 01:50:59 +0200

    I1BCIP-122: Added name and id to img uploader props types

--- src/components/img-uploader/types.ts
+++ src/components/img-uploader/types.ts
@@ -1,6 +1,8 @@
 import { ChangeEvent } from "react";
 
 export interface ImgUploaderProps {
+    id?:string;
+    name:string;
     onChange: (event: ChangeEvent<HTMLInputElement>) => void;
     defaultValue?: string;
     externalError?: string;
