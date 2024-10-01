aa307fa6448ca492cfdd6bb7a2c91fe3afce688f
whbe-yossef <whbe.yossef@motopp.nl>
2024-10-01 22:33:22 +0200

    I1BCIP-120: create-basic-right-section

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,8 +1,13 @@
 import { FC } from 'react';
 import './styles.css';
 import { UserDataProps } from './types';
+import { MdEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa";
+import { IoLocationSharp } from "react-icons/io5";
+import { Typography } from '@mui/material';
 
 const ProfilePage: FC<UserDataProps> = ({ userData }) => {
+  console.log(userData)
   return (
     <div className="profile-page-container">
       <div className="profile-page-header">
@@ -10,7 +15,16 @@ const ProfilePage: FC<UserDataProps> = ({ userData }) => {
       </div>
       <div className="profile-page-body">
         <div className="profile-page-left-section"></div>
-        <div className="profile-page-right-section"></div>
+        <div className="profile-page-right-section"> 
+          <Typography className='profile-page-user-role' variant="h4" gutterBottom>
+            {userData.role}
+          </Typography>
+
+          <div className='profil-page-user-details-container'> <MdEmail className='profile-page-user-details-icon' /><Typography variant="h6">{userData.user_details.email}</Typography></div>
+          <div className='profil-page-user-details-container'><FaPhone className='profile-page-user-details-icon' /><Typography variant="h6">{userData.phone}</Typography></div>
+          <div className='profil-page-user-details-container'><IoLocationSharp className='profile-page-user-details-icon' /><Typography variant="h6">{`${userData.address.street}, ${userData.address.postcode}, ${userData.address.city}, ${userData.address.state}, ${userData.address.country}`}</Typography></div>
+          
+        </div>
       </div>
     </div>
   );
