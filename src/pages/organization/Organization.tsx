6ee833f187153dff702aa7b3f1b4c51b46b314f6
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-10-09 17:36:25 +0200

    I1BCIP-66: Connected the Organization page to backend and handling its errors

--- src/pages/organization/Organization.tsx
+++ src/pages/organization/Organization.tsx
@@ -10,18 +10,38 @@ import {
 import { useNavigate } from 'react-router-dom';
 import { FaPlus } from 'react-icons/fa6';
 import './styles.css';
+import { fetchData } from '../../components/FetchData';
+import { OrgUrl } from '../../services/ApiUrls';
 
 export default function Organization() {
   const navigate = useNavigate();
 
   const [orgName, setOrgName] = useState<string | null>(null);
-  const [orgErrors, setOrgErrors] = useState<any>(null);
+  const [orgErrors, setOrgErrors] = useState<string | null>(null);
 
   useEffect(() => {
     if (!localStorage.getItem('Token')) {
       navigate('/login');
     }
-  }, []);
+  }, [navigate]);
+
+  const addOrganization = () => {
+    fetchData(`${OrgUrl}/`, 'POST', JSON.stringify({ name: orgName }), {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: localStorage.getItem('Token'),
+    })
+      .then((res) => {
+        if (res?.error) {
+          setOrgErrors(res?.errors?.name[0]);
+        } else if (res.status === 201) {
+          setOrgErrors('');
+          localStorage.setItem('org', res.org.id);
+          navigate('/', { replace: true });
+        }
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
     <Stack className="organization-page-container">
@@ -40,12 +60,12 @@ export default function Organization() {
             value={orgName || ''}
             onChange={(e) => setOrgName(e.target.value)}
             size="small"
-            helperText={orgErrors?.org?.[0] ? orgErrors?.org[0] : ''}
-            error={!!orgErrors?.org?.[0]}
+            helperText={orgErrors || ''}
+            error={!!orgErrors}
           />
           <Button
             className="organization-page-add-form-btn"
-            onClick={() => console.log(orgName)}
+            onClick={() => addOrganization()}
             variant="contained"
           >
             <FaPlus size={'28px'} />
