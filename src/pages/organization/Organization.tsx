85ae417e7574559dce1cfd668c1fe3ad545c319a
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-10-09 14:02:12 +0200

    I1BCIP-66: Fixed the view of organization page

--- src/pages/organization/Organization.tsx
+++ src/pages/organization/Organization.tsx
@@ -1,59 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Container } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
-import OrganizationModal from './OrganizationModal';
-import '../../styles/style.css'
-
-interface Item {
-    org: {
-        id: any;
-        name: any;
-    };
-}
+import React, { useEffect, useState } from 'react';
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { FaPlus } from 'react-icons/fa6';
+import './styles.css';
 
 export default function Organization() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const [organizationModal, setOrganizationModal] = useState(false)
+  const [orgName, setOrgName] = useState<string | null>(null);
+  const [orgErrors, setOrgErrors] = useState<any>(null);
 
-    useEffect(() => {
-        if (!localStorage.getItem('Token')) {
-            navigate('/login')
-        } else {
-            setOrganizationModal(true)
-        }
-    }, [])
-
-    const handleClose = (event: any, reason: any) => {
-        if (reason && reason == "backdropClick") {
-            return;
-        }
+  useEffect(() => {
+    if (!localStorage.getItem('Token')) {
+      navigate('/login');
     }
+  }, []);
 
-    return (
-        <Box>
-            <Container sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <OrganizationModal
-                    open={organizationModal}
-                    handleClose={handleClose}
-                />
-                {/* <Card>
-                    <List>
-                        {
-                            organization?.length > 0 &&
-                            organization.map((item, i) => (
-                                <ListItem key={i}>
-                                    <StyledListItemButton onClick={() => selectedOrganization(item?.org?.id)}>
-                                        <StyledListItemText>
-                                            {item?.org?.name}
-                                        </StyledListItemText>
-                                    </StyledListItemButton>
-                                </ListItem>
-                            ))}
-                    </List>
-                </Card> */}
-            </Container>
+  return (
+    <Stack className="organization-page-container">
+      <Container className="organization-page-main">
+        <Typography className="organization-page-title">
+          Add Organization
+        </Typography>
+        <Typography className="organization-page-sub-title">
+          Enter the name of your organization to continue.
+        </Typography>
+        <Box className="organization-page-add-form">
+          <TextField
+            className="organization-page-org-name-input"
+            label="organisations Name"
+            name="org"
+            value={orgName || ''}
+            onChange={(e) => setOrgName(e.target.value)}
+            size="small"
+            helperText={orgErrors?.org?.[0] ? orgErrors?.org[0] : ''}
+            error={!!orgErrors?.org?.[0]}
+          />
+          <Button
+            className="organization-page-add-form-btn"
+            onClick={() => console.log(orgName)}
+            variant="contained"
+          >
+            <FaPlus size={'28px'} />
+          </Button>
         </Box>
-
-    )
+      </Container>
+    </Stack>
+  );
 }
