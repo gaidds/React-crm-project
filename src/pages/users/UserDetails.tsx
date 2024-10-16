0e67b72a803ef0cde252a0826c50823079b2cb56
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-10-16 16:02:45 +0200

    I1BCIP-153: Moved UserDetails.tsx types to the types file

--- src/pages/users/UserDetails.tsx
+++ src/pages/users/UserDetails.tsx
@@ -6,31 +6,7 @@ import { AntSwitch } from '../../styles/CssStyled';
 import { UserUrl } from '../../services/ApiUrls';
 import { fetchData } from '../../components/FetchData';
 import { useMyContext } from '../../context/Context';
-
-type response = {
-  user_details: {
-    email: string;
-    id: string;
-    is_active: boolean;
-    profile_pic: string;
-  };
-  role: string;
-  address: {
-    address_line: string;
-    street: string;
-    city: string;
-    state: string;
-    postcode: string;
-    country: string;
-  };
-  is_organization_admin: boolean;
-  has_marketing_access: boolean;
-  has_sales_access: boolean;
-  phone: string;
-  alternate_phone: string;
-  date_of_joining: string;
-  is_active: boolean;
-};
+import { response } from './types';
 
 export const formatDate = (dateString: any) => {
   const options: Intl.DateTimeFormatOptions = {
