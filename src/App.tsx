d79949001385fc51146ba5f484cd5181e47111b0
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-10-22 14:21:58 +0200

    I1BCIP-155: Moved all routes to the app component and organized them

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import { Home } from './pages/home/Home';
 import PasswordResetPage from './pages/auth/PasswordResetPage';
-import ForgotPassword from './pages/forgot-password/ForgotPassword'; // Import the ForgotPassword component
+import ForgotPassword from './pages/forgot-password/ForgotPassword';
 import ResetForgotPassword from './pages/forgot-password/ResetForgotPassword';
-import RegistrationPage from './pages/auth/RegistrationPage';import EditContacts from './pages/contacts/EditContacts';
-import TokenExpirationHandler from './components/TokenExpirationHandler'; 
-
+import RegistrationPage from './pages/auth/RegistrationPage';
+import TokenExpirationHandler from './components/TokenExpirationHandler';
+import Contacts from './pages/contacts/Contacts';
+import ContactDetails from './pages/contacts/ContactDetails';
+import Accounts from './pages/accounts/Accounts';
+import AccountDetails from './pages/accounts/AccountDetails';
+import Users from './pages/users/Users';
+import ProfilePage from './pages/profile/Profile';
+import Deals from './pages/deals/Deals';
+import DealsCardView from './pages/deals/DealsCardView';
+import DealDetails from './pages/deals/DealDetails';
 
 function App() {
-
   return (
     <>
       <Router>
         <TokenExpirationHandler />
         <Routes>
-          {/* Add new routes here */}
-          <Route path="/" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-
-          {/* Existing routes */}
           <Route path="*" element={<Home />} />
-          <Route path="/app" element={<Home />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/login" element={<Login />} />
           <Route
             path="/auth/reset-password/:uidb64/:token"
@@ -33,8 +35,19 @@ function App() {
             element={<ResetForgotPassword />}
           />
           <Route path="/auth/register" element={<RegistrationPage />} />
-          <Route path="/app/contacts/edit-contact/:contactId" element={<EditContacts />} />
 
+          <Route path="/app" element={<Home />}>
+            <Route path="contacts" element={<Contacts />} />
+            <Route path="contacts/:contactId" element={<ContactDetails />} />
+            <Route path="accounts" element={<Accounts />} />
+            <Route path="accounts/:accountId" element={<AccountDetails />} />
+            <Route path="users" element={<Users />} />
+            <Route path="user-details/:id" element={<ProfilePage />} />
+            <Route path="deals" element={<Deals />} />
+            <Route path="deals/card-view" element={<DealsCardView />} />
+            <Route path="deals/:dealId" element={<DealDetails />} />
+            <Route path="profile/:id" element={<ProfilePage />} />
+          </Route>
         </Routes>
       </Router>
     </>
