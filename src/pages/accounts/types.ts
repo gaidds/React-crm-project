a49b246619a2018a7b06dc7414dbb02f20ba4d26
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-10-28 11:50:22 +0100

    I1BCIP-155: Refactored AccountsDetailes page code and fixed it's layout

--- src/pages/accounts/types.ts
+++ src/pages/accounts/types.ts
@@ -8,3 +8,44 @@ export interface HeadCell {
 export type Item = {
     id: string;
   };
+
+export interface Account {
+  deals: string;
+  contacts: [
+    {
+      first_name: string;
+      last_name: string;
+      primary_email: string;
+      mobile_number: string;
+      department: string;
+      profile_pic: string;
+      address: {
+        street: string;
+        city: string;
+        state: string;
+        postcode: string;
+        country: string;
+      };
+    }
+  ];
+  description: string;
+  name: string;
+  website: string;
+  industry: string;
+  org: {
+    name: string;
+  };
+  assigned_to: [
+    {
+      user_details: {
+        email: string;
+      };
+    }
+  ];
+  tags: string[];
+  created_by: { id: string };
+}
+
+export interface Data {
+  deals: any[];
+}
