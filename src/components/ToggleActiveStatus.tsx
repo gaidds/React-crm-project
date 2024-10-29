8da21a1db856ff1d70224cb7212216a367d7d088
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-10-29 13:43:34 +0100

    I1BCIP-155: Cleaned TaggleActiveStatus component code

--- src/components/ToggleActiveStatus.tsx
+++ src/components/ToggleActiveStatus.tsx
@@ -4,86 +4,91 @@ import { Header, fetchData } from './FetchData';
 import { UserUrl } from '../services/ApiUrls';
 
 interface ToggleProps {
-    userId: string | undefined;
-    is_active: boolean | undefined;
+  userId: string | undefined;
+  is_active: boolean | undefined;
 }
 
 const ToggleActiveStatus = ({ userId, is_active }: ToggleProps) => {
-    const [isActive, setIsActive] = useState(is_active ?? true); 
-    const [loading, setLoading] = useState(false);
+  const [isActive, setIsActive] = useState(is_active ?? true);
+  const [loading, setLoading] = useState(false);
 
-    const fetchUserStatus = async () => {
-        if (!userId) return;
-        
-        try {
-            setLoading(true);
-            const response = await fetchData(`${UserUrl}/${userId}`, 'GET', null as any, Header);
-            if (response.ok) {
-                const data = await response.json();
-                setIsActive(data.profile_obj.is_active); 
-            } else {
-                console.error("Failed to fetch user status:", response.statusText);
-            }
-        } catch (error) {
-            console.error("Failed to fetch user status:", error);
-        } finally {
-            setLoading(false);
-        }
-    };
+  const fetchUserStatus = async () => {
+    if (!userId) return;
 
-    useEffect(() => {
-        fetchUserStatus();
-    }, [userId]);
-
-    const toggleActiveStatus = async () => {
-        if (!userId) return; 
-        const newStatus = isActive ? "Inactive" : "Active"; 
-        setIsActive(!isActive);
-
-        try {
-            const response = await fetchData(
-                `${UserUrl}/${userId}/status/`,
-                'POST',
-                JSON.stringify({ status: newStatus }),
-                Header
-            );
-
-            if (response.ok) {
-                await fetchUserStatus();
-            } else {
-                console.error("Failed to update status:", response.statusText);
-            }
-        } catch (error) {
-            console.error("Failed to update status:", error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    if (loading) {
-        return <CircularProgress />;
+    try {
+      setLoading(true);
+      const response = await fetchData(
+        `${UserUrl}/${userId}`,
+        'GET',
+        null as any,
+        Header
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setIsActive(data.profile_obj.is_active);
+      } else {
+        console.error('Failed to fetch user status:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Failed to fetch user status:', error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-        <FormControlLabel
-            control={
-                <Switch
-                    checked={isActive}  
-                    onChange={toggleActiveStatus}
-                    color="primary"
-                    sx={{
-                        '& .MuiSwitch-switchBase.Mui-checked': {
-                            color: '#65558F',
-                        },
-                        '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
-                            backgroundColor: '#65558F',
-                        },
-                    }}
-                />
-            }
-            label={isActive ? "User is Active" : "User is Inactive"}
+  useEffect(() => {
+    fetchUserStatus();
+  }, [userId]);
+
+  const toggleActiveStatus = async () => {
+    if (!userId) return;
+    const newStatus = isActive ? 'Inactive' : 'Active';
+    setIsActive(!isActive);
+
+    try {
+      const response = await fetchData(
+        `${UserUrl}/${userId}/status/`,
+        'POST',
+        JSON.stringify({ status: newStatus }),
+        Header
+      );
+
+      if (response.ok) {
+        await fetchUserStatus();
+      } else {
+        console.error('Failed to update status:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Failed to update status:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  return (
+    <FormControlLabel
+      control={
+        <Switch
+          checked={isActive}
+          onChange={toggleActiveStatus}
+          color="primary"
+          sx={{
+            '& .MuiSwitch-switchBase.Mui-checked': {
+              color: '#65558F',
+            },
+            '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+              backgroundColor: '#65558F',
+            },
+          }}
         />
-    );
+      }
+      label={isActive ? 'User is Active' : 'User is Inactive'}
+    />
+  );
 };
 
 export default ToggleActiveStatus;
