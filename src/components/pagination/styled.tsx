f67ae686f313e0e0af94f84436553d26efdf8d08
Ashraf Alshashaa <ashraf.alchechae@gmail.com>
2024-11-05 17:47:58 +0100

    I1BCIP-157: Created Pagination component

---
+++ src/components/pagination/styled.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+import { Box, Select, Stack, Typography } from '@mui/material';
+
+export const PaginationContainer = styled(Stack)``;
+
+export const PerPageSelect = styled(Select)``;
+
+export const PageNimContainer = styled(Box)``;
+
+export const PageNum = styled(Typography)``;
+
+export const selectOpenIconStyles = {};
+
+export const selectPagesIconStyles = {};
